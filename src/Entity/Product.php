945c09c641ff595a7dd3f3ab6fc60faf9af25903
Ngochb2809 <ngochbgbh211287@fpt.edu.vn>
2022-08-13 12:07:39 +0700

    Update 1

---
+++ src/Entity/Product.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProductRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ProductRepository::class)]
+class Product
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $productid;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $name;
+
+    #[ORM\Column(type: 'float')]
+    private $price;
+
+    #[ORM\Column(type: 'string', length: 500)]
+    private $description;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $image;
+
+    #[ORM\ManyToOne(targetEntity: Category::class, inversedBy: 'product')]
+    #[ORM\JoinColumn(nullable: false)]
+    private $catid;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getProductid(): ?string
+    {
+        return $this->productid;
+    }
+
+    public function setProductid(string $productid): self
+    {
+        $this->productid = $productid;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getPrice(): ?float
+    {
+        return $this->price;
+    }
+
+    public function setPrice(float $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(string $image): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    public function getCatid(): ?Category
+    {
+        return $this->catid;
+    }
+
+    public function setCatid(?Category $catid): self
+    {
+        $this->catid = $catid;
+
+        return $this;
+    }
+}
