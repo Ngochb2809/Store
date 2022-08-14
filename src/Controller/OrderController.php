5b038c554b3af9c5563f0735012f97ea4ea83fdd
Ngochb2809 <ngochbgbh211287@fpt.edu.vn>
2022-08-14 18:05:48 +0700

    Update order

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -2,18 +2,25 @@
 
 namespace App\Controller;
 
+use App\Entity\Order;
 use App\Entity\Product;
+use App\Controller\OrderController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class OrderController extends AbstractController
 {
+    public function __construct()
+    {
+        $this->session = new Session();
+    }
     #[Route('/cart/info', name: 'add_to_cart')]
     public function addToCart (Request $request) {
        //khởi tạo session
-       $session = $request->getSession();
+       $this->session = $request->getSession();
        //lấy dữ liệu gửi từ form Add To Cart
        $product = $this->getDoctrine()->getRepository(Product::class)->find($request->get('id'));
        $quantity = $request->get('quantity');
@@ -27,11 +34,11 @@ class OrderController extends AbstractController
        $productprice = $product->getPrice();
        $totalprice = $productprice * $quantity;
        //set biến session (global variable) để lưu dữ liệu 
-       $session->set('product', $product);
-       $session->set('user', $user);
-       $session->set('quantity', $quantity);
-       $session->set('totalprice', $totalprice);
-       $session->set('datetime', $datetime);
+       $this->session->set('product', $product);
+       $this->session->set('user', $user);
+       $this->session->set('quantity', $quantity);
+       $this->session->set('totalprice', $totalprice);
+       $this->session->set('datetime', $datetime);
        return $this->render('cart/detail.html.twig');
     }
 
@@ -41,13 +48,22 @@ class OrderController extends AbstractController
         //khởi tạo session;
         $session = new Session();
         //tạo object Order
-        
+        $order = new Order;
         //set các thuộc tính cho object Order
-
+        $order = $session->get('product');
+        $order = $session->get('user');
+        $order = $session->get('quantity');
+        $order = $session->get('totalprice');
+        $order = $session->get('datetime');
+        return $this->render('order/detail.html.twig');
         //lưu object Order vào DB bằng Manager
-
+        $manager = $this->getDoctrine()->getManager();
+        $manager->persist($order);
+        $manager->flush();
         //gửi thông báo về trang Store bằng addFlash()
-
+        $this->addFlash('Info','Buy successs!');
+        return $this->redirectToRoute("product_store");
         //quay về trang Store bằng RedirectToRoute()
+
     }
 }
